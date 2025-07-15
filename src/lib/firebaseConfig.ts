fc2022fefd19011bf45462ac5b932b765ed625c5
Aliuddin002 <127687447+Aliuddin002@users.noreply.github.com>
2025-07-15 23:19:16 +0500

    Update firebaseConfig.ts

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -1,9 +1,4 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyC1LuIndn6aKU4MuPMldYEmLkheqFOOAl0",
-  authDomain: "muzeb2.firebaseapp.com",
-  projectId: "muzeb2",
-  storageBucket: "muzeb2.firebasestorage.app",
-  messagingSenderId: "112282090793",
-  appId: "1:112282090793:web:99a5c799021d51b2b10b38"
+
 };
